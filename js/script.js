84ed813e86a00d8d6c933f9fc41cae2fc5dcb6b9
Fadil939 <fadhlurrahman939@gmail.com>
2023-02-13 15:57:25 +0700

    membuat webside kedai kopi

---
+++ js/script.js
@@ -0,0 +1,13 @@
+//toggle active
+const navbarNav = document.querySelector(".navbar-nav");
+document.querySelector("  #hamburger-menu ").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+//ketika kita klick di luar contais nav akan hilang
+const hamburger = document.querySelector("#hamburger-menu");
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
